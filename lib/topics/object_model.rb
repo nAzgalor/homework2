4a6db40923213cdba96f9b61e3effd17b785b26d
nAzgalor <nazgalor@gmail.com>
2017-06-10 19:03:40 +0300

    homework2 nazar.hlynskyi

--- lib/topics/object_model.rb
+++ lib/topics/object_model.rb
@@ -2,4 +2,26 @@
 
 # exercise object model
 module ObjectModel
+  # Linux
+  module LinuxFriendly
+    def fork_process
+      super
+    end
+  end
+
+  # Desktop
+  class Desktop
+    def fork_process
+      'Parent: allocate memory'
+    end
+
+    def mine_bitcoins
+      inspect
+    end
+  end
+
+  # Laptop
+  class Laptop < Desktop
+    prepend LinuxFriendly
+  end
 end
--- lib/topics/state_and_behaviour.rb
+++ lib/topics/state_and_behaviour.rb
@@ -2,4 +2,27 @@
 
 # exercise state and behaviour
 module StateAndBehaviour
+  # basic object
+  class Car
+    attr_accessor :color, :model, :year, :current_speed
+
+    def initialize(params)
+      @color = params[:color] || 'color'
+      @model = params[:model] || 'model'
+      @year  = params[:year] || 2000
+      @current_speed = 0
+    end
+
+    def push_break(speed)
+      @current_speed -= speed if @current_speed >= speed
+    end
+
+    def speed_up(speed)
+      @current_speed += speed
+    end
+
+    def self.default_car
+      Car.new
+    end
+  end
 end
